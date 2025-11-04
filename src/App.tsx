eea2725b5e9ef7ee3267106c8d3ce0447af0fc6c
TelmoPersch <119141423+telmopersch123@users.noreply.github.com>
2025-11-03 21:20:08 -0400

    feat: implementação do shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import { ArrowUpIcon } from 'lucide-react'
 import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
   return (
     <>
       <div className="min-h-screen bg-black/20">
-        <p className="m-10 text-white">teste</p>
+        <Button variant="outline">Button</Button>
+        <Button variant="outline" size="icon" aria-label="Submit">
+          <ArrowUpIcon />
+        </Button>
       </div>
     </>
   )
