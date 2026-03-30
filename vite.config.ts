d0c4f022183c9eb9cb96e8666144c3942475abde
TelmoPersch <119141423+telmopersch123@users.noreply.github.com>
2026-03-30 19:45:00 -0400

    build: correção nos vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,24 @@
+// import react from '@vitejs/plugin-react'
+// import fs from 'fs'
+// import path from 'path'
+// import { defineConfig } from 'vite'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       '@': path.resolve(__dirname, './src'),
+//     },
+//   },
+//   server: {
+//     https: {
+//       key: fs.readFileSync('./certs/localhost+2-key.pem'),
+//       cert: fs.readFileSync('./certs/localhost+2.pem'),
+//     },
+//     port: 5375, // pode ser qualquer porta que você quiser
+//   },
+// })
 import react from '@vitejs/plugin-react'
-import fs from 'fs'
 import path from 'path'
 import { defineConfig } from 'vite'
 
@@ -11,10 +30,6 @@ export default defineConfig({
     },
   },
   server: {
-    https: {
-      key: fs.readFileSync('./certs/localhost+2-key.pem'),
-      cert: fs.readFileSync('./certs/localhost+2.pem'),
-    },
-    port: 5375, // pode ser qualquer porta que você quiser
+    port: 5375,
   },
 })
